be82e7dd8f6ef6335a2e0ef42c8908c726f1d091
SagarPrasad160 <sagarprasad916@gmail.com>
2023-07-17 23:12:36 +0530

    prepare for deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 // Connect Databse
 const connectDB = require("./config/db");
 connectDB();
@@ -10,13 +10,19 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to Home of Contact Keeper" });
-});
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
