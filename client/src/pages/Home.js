8fe6a8af7c85dace9f99d38edf8528689e971acc
SagarPrasad160 <sagarprasad916@gmail.com>
2023-07-17 23:24:53 +0530

    remove loading from Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 const Home = () => {
-  const { isAuthenticated, loading } = useContext(AuthContext);
+  const { isAuthenticated } = useContext(AuthContext);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
